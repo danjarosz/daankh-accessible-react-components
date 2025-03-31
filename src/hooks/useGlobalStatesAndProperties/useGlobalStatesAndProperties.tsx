e0f434b250306478cca6d444e8bb1ebb80fb8f91
Daniel Jarosz <jarosz.daniel@protonmail.com>
2025-03-31 21:26:33 +0200

    update exports

--- src/hooks/useGlobalStatesAndProperties/useGlobalStatesAndProperties.tsx
+++ src/hooks/useGlobalStatesAndProperties/useGlobalStatesAndProperties.tsx
@@ -25,8 +25,6 @@ export default function useGlobalStatesAndProperties({
     }
   }, [atomic]);
 
-  // TODO Update docs
-
   return {
     'aria-atomic': ariaAtomic,
   };
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-export { default as Button } from './components/button/index.js';
+// Generic
+export { default as useGlobalStatesAndProperties } from './hooks/useGlobalStatesAndProperties/useGlobalStatesAndProperties.js';
+
+// Hooks
 export { default as useAccessibleButton } from './hooks/useAccessibleButton/useAccessibleButton.js';
+
+// Components
+export { default as Button } from './components/button/index.js';
