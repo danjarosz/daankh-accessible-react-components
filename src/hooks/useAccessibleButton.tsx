5832f75c1ff7ad9d3e37fb0458132eaf231c5a76
Daniel Jarosz <jarosz.daniel@protonmail.com>
2025-03-31 19:04:39 +0200

    fix: testing

--- src/hooks/useAccessibleButton.tsx
+++ src/hooks/useAccessibleButton.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export interface Props {
   disabled?: boolean;
@@ -7,8 +7,6 @@ export interface Props {
 export interface IUseAccessibleButton {
   'aria-disabled': 'true' | 'false' | undefined;
 }
-
-// TODO: checkout usage of @storybook/addon-a11y and how tests via storybook works. Before I removed their config.
 
 export default function useAccessibleButton({
   disabled = false,
